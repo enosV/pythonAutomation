755d5cd70b6d69549ce0a32359fa003bfc888072
enosv <enosvazquez@gmail.com>
2023-01-05 13:48:23 -0800

    update

--- getNews.py
+++ getNews.py
@@ -26,3 +26,13 @@ def extract_news(url):
     response = requests.get(url)
     content = response.content
     soup = BeautifulSoup(content, 'html.parser')
+    for i, tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
+        cnt += ((str(i+1)+' :: '+tag.text + "\n" + '<br>') if tag.text!='More' else '')
+        #print(tag.prettify) #find all('span', attrs={'class':'sister'})
+    return(cnt)
+
+    cnt = extract_news('https://news.ycombinator.com/')
+    content += cnt
+    content += ('<br>-----<br>')
+    content += ('<br><br>End of Message')
+
